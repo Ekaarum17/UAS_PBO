1d3a353381b58c864f21095cd085b5e73edf66e4
Ekaarum17 <siiarum17@gmail.com>
2023-01-15 19:56:05 +0800

    first commit

---
+++ FDAe020320095/FDAe020320095/app/src/main/java/id/ac/poliban/E020320095/project/Activity/loginActivity.java
@@ -0,0 +1,4 @@
+package id.ac.poliban.E020320095.project.Activity;
+
+public class loginActivity {
+}
---
+++ FDAe020320095/FDAe020320095/app/src/main/java/id/ac/poliban/E020320095/project/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.E020320095.project.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
